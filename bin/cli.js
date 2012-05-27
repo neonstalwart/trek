4aea6f63abdbb12d4920b9b727b76e7e6e71cf3e
ben hockey <neonstalwart@gmail.com>
2012-05-27 15:43:10 -0400

    cli: add usage info, parse args and initiate migrations

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,38 @@
 #!/usr/bin/env node
 
-var trek = require('../');
+var usage = [
+		'migrate data',
+		'',
+		'Usage:',
+		// TODO: add a -g option for generating an empty migration file
+		'$0 [-p <path>] [<migration> | up | down | latest]',
+		'',
+		'Options:',
+		'<path>',
+		'	path to application root.  path should contain a trek directory with the migrations.',
+		'	default is current working directory.',
+		'<migration>',
+		'	the sequence number of the specific migration to migrate to.',
+		'up',
+		'	migrate up to next migration',
+		'down',
+		'	migrate down from current migration',
+		'latest',
+		'	migrate to the migration with the largest sequence number.',
+		'	this is the default behavior if no othe migration is specified'
+	].join('\n'),
+	argv = require('optimist')
+		.usage(usage)
+		['default']('p', process.cwd())
+		.argv,
+	path = require('path'),
+	trekDir = path.join(argv.p, 'trek'),
+	migrationSpec = argv._[0] || 'latest',
+	// NODE_ENV can be used to switch configuration.  default is 'dev'
+	env = process.env.NODE_ENV || 'dev',
+	// use the config for this environment
+	config = require(path.join(trekDir, 'config'))[env],
+	trek = require('../');
+
+// migrate the data
+trek.hike(trekDir, migrationSpec, config);
